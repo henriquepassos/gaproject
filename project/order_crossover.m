1bf3b8d706742b9726858c7fad8915e4e131cf4e
maartn <maarten.christiaen@gmail.com>
2013-12-05 15:51:23 +0100

    Order crossover bug fixed
    Fixed order crossover bug and added to gui.

--- project/order_crossover.m
+++ project/order_crossover.m
@@ -7,20 +7,20 @@
 %    Parents         - The parents' chromosomes
 
 function Offspring=order_crossover(Parents)
-    chromSize=size(Parents,2)
+    chromSize=size(Parents,2);
     Offspring = zeros(1,chromSize);
     rndi=zeros(1,2);
-
+    
     while rndi(1)==rndi(2)
         rndi=rand_int(1,2,[1 chromSize]);
     end
     rndi = sort(rndi);
-    Offspring(rndi(1):rndi(2))= Parents(1,randi(1):rndi(2));
+    Offspring(rndi(1):rndi(2))= Parents(1,rndi(1):rndi(2));
     for i=1:chromSize
         if i>=rndi(1) && i<=rndi(2)
             continue;
         end
-        for j=i:chromSize
+        for j=1:chromSize
             candidate = Parents(2,j);
             if ismember(candidate,Offspring)
                 continue;
