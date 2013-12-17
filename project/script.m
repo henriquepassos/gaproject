ecff7bbf5e002e8e998a10b16413ace3d1840bc1
SITUBUN <steven.vanessche@gmail.com>
2013-12-17 16:56:22 +0100

    jkj

--- project/script.m
+++ project/script.m
@@ -35,7 +35,7 @@ for k = 1 : 3
 
     j = 1;
     for i = 0 : step : 1
-        means(j) = run(times, x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, i, 0.25, OPT, CROSSOVER, MUTATION, SELECTION, LOCALLOOP, ah1, ah2, ah3 );
+        means(j) = run(times, x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, i, 0.05, OPT, CROSSOVER, MUTATION, SELECTION, LOCALLOOP, ah1, ah2, ah3 );
         j = j + 1;
     end
 
